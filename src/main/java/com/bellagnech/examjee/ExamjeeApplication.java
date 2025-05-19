4f6d6f082779155faf6878aeb18e6348519ea93f
Abdelkrim7Be <119794076+Abdelkrim7Be@users.noreply.github.com>
2025-05-19 09:18:01 +0100

    Initial Project Setup & Structure Added

---
+++ src/main/java/com/bellagnech/examjee/ExamjeeApplication.java
@@ -0,0 +1,13 @@
+package com.bellagnech.examjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamjeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamjeeApplication.class, args);
+	}
+
+}
